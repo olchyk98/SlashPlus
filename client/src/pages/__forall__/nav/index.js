1a3fbddcb3675d17c4c32271586da4dd8a01ea9d
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-03-15 17:41:39 +0100

    Home > Color palettes tempalte

--- client/src/pages/__forall__/nav/index.js
+++ client/src/pages/__forall__/nav/index.js
@@ -1,19 +1,91 @@
 import React, { Component } from 'react';
-import './main.scss';
+import './main.css';
+
+import { Link } from 'react-router-dom';
+
+import links from '../../../links';
+
+// https://css-tricks.com/building-progress-ring-quickly/
+class ProgressRing extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        	progress: 0
+        }
+
+        const { radius, stroke } = this.props;
+
+        this.normalizedRadius = radius - stroke * 2;
+        this.circumference = this.normalizedRadius * 2 * Math.PI;
+
+        this.inAnimation = this.amDown = false;
+    }
+
+    componentDidMount() {
+    	this.inAnimation = true;
+    	this.progressCircle();
+    }
+
+    componentWillUnmount() {
+    	clearInterval(this.animationINT);
+    	this.inAnimation = this.amDown = false;
+    }
+
+    progressCircle = () => {
+    	if(!this.inAnimation) return;
+
+    	this.setState(({ progress: a }) => ({
+    		progress: a + 2.5
+    	}));
+    	requestAnimationFrame(this.progressCircle);
+    }
+
+    render() {
+        const { radius, stroke } = this.props;
+        const strokeDashoffset = this.circumference - (this.state.progress / 100) * this.circumference;
+
+        return (
+            <svg height={ radius * 2 } width={ radius * 2 }>
+                <circle
+                    stroke="rebeccapurple"
+                    fill="transparent"
+                    strokeWidth={ stroke }
+                    strokeDasharray={ this.circumference + " " + this.circumference }
+                    style={{ strokeDashoffset }}
+                    r={ this.normalizedRadius }
+                    cx={ radius }
+                    cy={ radius }
+                />
+            </svg>
+        );
+    }
+}
 
 class Hero extends Component {
+	constructor(props) {
+		super(props);
+
+		this.lCircleSize = 40
+	}
+
 	render() {
 		return(
 			<nav className="gl-nav">
-				<span className="rn-nav-logo">
-					Elements
-				</span>
-				<div className="rn-nav-ldots">
-					<div />
-					<div />
-					<div />
+				<Link to={ links["HOME_PAGE"].absolute } className="rn-nav-logo">
+					Home
+				</Link>
+				<div className="rn-nav-loading">
+					{
+						(true) ? null : (
+							<ProgressRing
+								radius={ this.lCircleSize / 2 }
+								stroke={ 4 }
+							/>
+						)
+					}
 				</div>
-				<span>Mark Stalker</span>
+				<Link to="/">Mark Stalker</Link>
 			</nav>
 		);
 	}
--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
-import './main.scss';
+import './main.css';
+
+import { Link } from 'react-router-dom';
+
+import links from '../../links';
 
 class Hero extends Component {
 	render() {
 		return(
 			<div className="rn rn-sections">
-				
+				{/* color palettes */}
+				<article className="rn-sections-item">
+					<Link to={ "/" } className="rn-sections-item-title">Color palettes</Link>
+					<div className="rn-sections-item_split" />
+					<div className="rn-sections-item-content grid">
+						<div className="rn-sections-item-palette">
+							{
+								[
+									"#624480",
+									"#714E93",
+									"#865DAF",
+									"#A96DD1",
+									"#B57EDC"
+								].map((session, index) => (
+									<div key={ index } style={{ background: session }} />
+								))
+							}
+						</div>
+						<div className="rn-sections-item-palette">
+							{
+								[
+									"#261E1B",
+									"#373938",
+									"#26A96C",
+									"#F4DDC8",
+									"#EC6C5B",
+									"#F7B304"
+								].map((session, index) => (
+									<div key={ index } style={{ background: session }} />
+								))
+							}
+						</div>
+						<div className="rn-sections-item-palette">
+							{
+								["#f0d8a8","#3d1c00","#86b8b1","#f2d694","#fa2a00"].map((session, index) => (
+									<div key={ index } style={{ background: session }} />
+								))
+							}
+						</div>
+						<div className="rn-sections-item-palette">
+							{
+								["#C2412D","#D1AA34","#A7A844","#A46583","#5A1E4A"].map((session, index) => (
+									<div key={ index } style={{ background: session }} />
+								))
+							}
+						</div>
+					</div>
+				</article>
+				{/* colours */}
+				{/* fonts */}
+				{/* articles */}
 			</div>
 		);
 	}
