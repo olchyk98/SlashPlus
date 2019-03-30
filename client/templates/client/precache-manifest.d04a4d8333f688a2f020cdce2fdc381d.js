5f82afabc321f0e303985fb7b0ea9a662ad5d585
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-03-30 14:20:04 +0100

    MS

---
+++ client/templates/client/precache-manifest.d04a4d8333f688a2f020cdce2fdc381d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5d6d7bd9c0d37609a4f",
+    "url": "/static/js/main.08d7073e.chunk.js"
+  },
+  {
+    "revision": "2027c68282256f6aaa1f",
+    "url": "/static/js/2.6d5fcb11.chunk.js"
+  },
+  {
+    "revision": "a5d6d7bd9c0d37609a4f",
+    "url": "/static/css/main.23a36084.chunk.css"
+  },
+  {
+    "revision": "61c2fe2b8c9e92fa655e08705758809f",
+    "url": "/index.html"
+  }
+];
